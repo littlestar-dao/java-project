7956d862c1996e1f388dd480ec564b3af433a710
littlestar-dao <dao.duan@hotmail.com>
2017-07-21 15:43:10 +0700

    start_game_should_love_all() is done

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -2,18 +2,27 @@ package exam1;
 
 public class Exam01 {
 
+
+
     public Exam01(String firstPerson, String secondPerson) {
     }
 
 
+
     public void playerAWin() {
+
+
     }
 
     public void playerBWin() {
+
+
     }
 
     public String getScore() {
-        return null;
+
+
+        return "Love-All";
     }
 
 }
